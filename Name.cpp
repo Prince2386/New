ada450e7cdbc3fd50325be64e6c38c1ced391c3f
Prince2386 <154878338+Prince2386@users.noreply.github.com>
2024-01-25 14:33:52 +0530

    first commit

---
+++ Name.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout<<"Enter the name :"<< endl;
+    cin>> name;
+    cout<<"\n The entered name is :"<< name;
+    return 0;
+
+}
